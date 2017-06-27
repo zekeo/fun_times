3d290170018862401412d1c0db005ae772813b48
zekeo <Zeke.spier@gmail.com>
2017-06-27 16:25:40 -0700

    Add files via upload

--- icanreadyourmind.py
+++ icanreadyourmind.py
@@ -11,32 +11,65 @@ print("do you have your number?")
 time.sleep(2)
 print(guess)
 
-h = "higher"
-l = "lower"
-c = "correct"
-
 response = ()
-maxguess = int(100)
-minguess = int(0)
+maxguess = (100)
+minguess = (0)
 
 
-while response != "correct":
-    response = (input("higher, lower or correct?"))
+while response not in ["correct", "c"]:
+    response = (input("higher, lower or correct? \n:"))
     guess = int(guess)
 
-    if response == "higher":
-        if guess > minguess:
-            minguess = guess
-        guess = int(round(((maxguess-guess)/2)+guess))
-        print("I bet your number is %s" % guess)
-
-    elif response == "lower":
-        if guess < maxguess:
-            maxguess = guess
-        guess = int(round(((guess-minguess) / 2)+minguess))
-        print("I bet your number is %d" % guess)
+    if ((guess == 100) or (guess == 0)):
+        print("You're a dirty rotten liar")
+        break
 
     else:
-        print("I don't understand, h, l, or c!?!?")
+        if (maxguess-minguess) >= 2:
+            if response in ["higher", "h"]:
+                if guess > minguess:
+                    minguess = guess
+                guess = int(round(((maxguess-guess)/2)+guess))
+                print("I bet your number is %s" % guess)
+
+            elif response in ["lower", "l"]:
+                if guess < maxguess:
+                    maxguess = guess
+                guess = int(round(((guess-minguess) / 2)+minguess))
+                print("I bet your number is %d" % guess)
+
+            else:
+                print("I don't understand, h, l, or c!?!?")
+
+        else:
+            print("oh I see you don't like intergers. Fine.")
+            time.sleep(1)
+            print("no more than one decimal though ok?")
+            time.sleep(1)
+
+            while response not in ["correct", "c"]:
+                response = (input("higher, lower or correct? \n:"))
+                guess = int(guess)
+
+                if minguess == maxguess:
+                    maxguess = (maxguess + .5)
+
+                if response in ["higher", "h"]:
+                    if guess > minguess:
+                        minguess = int(guess)
+                        guess = int(guess)
+                    guess = (round((((maxguess-guess)/2)+guess), 2))
+                    print("I bet your number is %s" % guess)
+
+                elif response in ["lower", "l"]:
+                    if guess < maxguess:
+                        maxguess = int(guess)
+                    guess = (round((((guess-minguess) / 2)+minguess), 2))
+                    print("I bet your number is %d" % guess)
+
+                else:
+                    print("I don't understand, h, l, or c!?!?")
+
+
 
 print("I knew I could read your mind!")
